f95ea531b8a8a017697352a0d4478faf5b3f4720
0xFF <dmitry.mikhaltsov98@gmail.com>
2025-03-18 15:28:53 +0300

    Add files via upload

---
+++ wps-defender.cmd
@@ -0,0 +1,3 @@
+powershell Set-ExecutionPolicy Bypass
+powershell wget -O wps-defender.ps1 https://raw.githubusercontent.com/bqio/wps-bullshit/refs/heads/main/wps-defender.ps1
+powershell ./wps-defender.ps1
