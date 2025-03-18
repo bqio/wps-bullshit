16816e79b50528d24c7e13baa343c1c272d5989b
0xFF <dmitry.mikhaltsov98@gmail.com>
2025-03-18 15:40:08 +0300

    Add files via upload

--- wps-defender.ps1
+++ wps-defender.ps1
@@ -1,8 +1,10 @@
-$Path = "$Env:LOCALAPPDATA\Kingsoft"
+Get-LocalUser | Select-Object Name | ForEach-Object {
+    $Path = "C:\Users\" + $_.Name + "\AppData\Local\Kingsoft"
 
-Write-Host "WPS Bullshit defender started."
+    Write-Host "WPS Bullshit defender started for $Path"
 
-New-Item -ItemType Directory -Force -Path $Path
-Get-Acl C:\Windows | Set-Acl $Path
+    New-Item -ItemType Directory -Force -Path $Path
+    Get-Acl C:\Windows | Set-Acl $Path
 
-Write-Host "WPS Bullshit defender ended."
+    Write-Host "WPS Bullshit defender ended."
+}
